83d76bafbe3ca8c1a60d799eb39cd1a5a60ded03
Jeff Lambert <jefflikeschicken@gmail.com>
2016-04-30 08:26:20 -0400

    Adding contains method to array prototype

--- src/jrl.utils.js
+++ src/jrl.utils.js
@@ -125,4 +125,14 @@
             }
         }
     ]);
+
+    Array.prototype.contains = function(needle) {
+        for(var i in this) {
+            if(this.hasOwnProperty(i) && this[i] === needle) {
+                return true;
+            }
+        }
+
+        return false;
+    };
 })();
