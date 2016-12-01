816c5dcd9b38aaaf68eb732310edb5500173933d
Jeff Lambert <jeflambe@cisco.com>
2016-12-01 09:55:59 -0500

    Changing name to angular-utils

--- src/angular-utils.js
+++ src/angular-utils.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
 
-    var app = angular.module('angular.utils', []);
+    var app = angular.module('angular-utils', []);
 
     app.factory('common', [
         '$window',
