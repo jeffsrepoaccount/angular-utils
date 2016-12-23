a091dc68f81f31e1fd8adf10d3a9bc254bef162d
Jeff Lambert <jefflikeschicken@gmail.com>
2016-12-22 20:17:35 -0500

    Merge from local. Removing toastr because angular-toastr.

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
-# jrl-utils
+# jrl-angular-utils
 
-Includes utility services for interacting with localStorage, sessionStorage as well as better client-side logging than `console.log`.
+Includes utility services for interacting with localStorage, sessionStorage as well as better client-side logging than `console.log`. 
+
+## Installation
+
+Install using NPM:
+
+```bash
+$ npm install jrl-angular-utils
+```
+
+or using bower:
+
+```bash
+$ bower install jeffsrepoaccount/jrl-angular-utils
+```
+
+Include in page:
+
+<script type="text/javascript" src="/{public_path}/dist/angular-utils.min.js"></script>
+
+Or be sure to include the file `{path}/jrl-angular-utils/dist/angular-utils.min.js` into your build process.
 
 ## Usage
 
 ```javascript
-angular.module('myModule', ['jrl.utils'])
+angular.module('myModule', ['angular-utils'])
     .controller('MyCtrl', [
-        'localStorage', 'sessionStorage', 'notify', 'common',
-        function(localStorage, sessionStorage, notify, common) {
-            // Logging
-            // Adds a locality parameter to each log attempt to better help 
-            // track down bugs or see where log statements are being generated 
-            // from. Timestamps are also appended to each log call
+        'localStorage', 'sessionStorage', 'common',
+        function(localStorage, sessionStorage, common) {
+
+            // Local references to logging functions
             var logInfo     = common.getLogFn('Locality', 'info'),
                 logSuccess  = common.getLogFn('Locality', 'success'),
                 logError    = common.getLogFn('Locality', 'error'),
@@ -28,28 +46,6 @@ angular.module('myModule', ['jrl.utils'])
             sessionStorage.setObject('another_key', { some: 'object' });
             var obj = sessionStorage.getObject('another_key');
             logWarn('Retrieved another_key from sessionStorage', myObj);
-
-            // toastr is also supported
-            common.toast('I am a success toast!');
-            common.toastSuccess('Something else was successful!', 'Titles Sometimes Help');
-            common.toastInfo('Something informative happened', 'FYI');
-            common.toastWarning('Careful now...', 'Attention');
-            common.toastError('Something has broken!', 'Oh no!');
-            // toasts are automatically piped through to the associated log function,
-            // the only difference being the locality will be '(toast)'
-
-			// Notifications
-            // You can either request permission to send desktop notifications first,
-            // or just go ahead and try sending something. Either way permission 
-            // will be checked.  If notifications are not supported, nothing will 
-            // happen
-            notify.send('Title', 'Body', 'icon.jpg', 'ltr');
-
-            notify.requestPermission().then(
-                function() {
-                    notify.send('Title', 'Body', 'icon.jpg', 'ltr');
-                }
-            );
         }
     ])
 ;
@@ -57,14 +53,10 @@ angular.module('myModule', ['jrl.utils'])
 
 This package also provides a few extra object functions.
 
-`Object.isEmpty()`          - Returns true if an object is empty, false otherwise.
+`Array.prototype.contains(needle)`  - Returns true if array contains `needle`, false otherwise.
 
-`Object.merge(o2)`          - Merges `o2` with Object. `o2` keys will overwrite `this`.
+`Array.prototype.intersect(array)`  - Calculates array intersection and returns array as a result.
 
-`Array.contains(needle)`    - Returns true if Array contains `needle`, false otherwise.
+`Date.prototype.addDays(days)`      - Adds `days` to a `Date`. Returns the resulting `Date`.
 
-`Array.intersect(array)`    - Calculates array intersection and returns array as a result.
-
-`Date.addDays(days)`        - Adds `days` to a `Date`. Returns the resulting `Date`.
-
-`Date.subDays(days)`        - Substracts `days` from a `Date`. Returns the resulting `Date`.
+`Date.prototype.subDays(days)`      - Substracts `days` from a `Date`. Returns the resulting `Date`.
--- src/angular-utils.js
+++ src/angular-utils.js
@@ -8,27 +8,11 @@
 (function() {
     'use strict';
 
-    var app = angular.module('angular-utils', ['toastr']);
-
-    app.config([
-        'toastrConfig',
-        function(toastrConfig) {
-            // TODO: Move these values to config?
-            angular.extend(toastrConfig, {
-                newestOnTop      : false,
-                closeMethod      : 'fadeOut',
-                closeDuration    : 300,
-                closeEasing      : 'swing',
-                timeOut          : 7500,
-                extendedTimeOut  : 10000,
-                positionClass    : 'toast-bottom-right'
-            });
-        }
-    ]);
+    var app = angular.module('angular-utils', []);
 
     app.factory('common', [
-        '$window', 'toastr', 
-        function($window, toastr) {
+        '$window',
+        function($window) {
             var enableDebugLogging = true;
             // Default log functions to empty
             var logFn       = function(){},
@@ -53,10 +37,6 @@
                 isMobileOrTablet:   isMobileOrTablet,
                 queryStringToJson:  queryStringToJson,
                 debounce:           debounce,
-                toast:              toastSuccess,
-                toastInfo:          toastInfo,
-                toastWarning:       toastWarning,
-                toastError:         toastError,
                 setLogger:          setLogger,
                 setInfoLogger:      setInfoLogger,
                 setWarnLogger:      setWarnLogger,
@@ -64,10 +44,10 @@
                 setLoggingStatus:   setLoggingStatus
             };
 
-            var logSuccess  = getLogFn('utils', 'success'),
-                logInfo     = getLogFn('utils', 'info'),
-                logWarn     = getLogFn('utils', 'warn'),
-                logError    = getLogFn('utils', 'error')
+            var logSuccess = getLogFn('utils', 'success'),
+                logInfo = getLogFn('utils', 'info'),
+                logWarn = getLogFn('utils', 'warn'),
+                logError = getLogFn('utils', 'error')
             ;
 
             return svc;
@@ -108,36 +88,26 @@
                 }
             }
 
-            function toastSuccess(message, title) {
-                toastr.success(message, title ? title : '');
-                logSuccess('(toast) ' + message, title);
-            }
+            // isEmptyObject from @Christoph: http://stackoverflow.com/a/679937/697370
+            function isEmptyObject(obj) {
+                for(var prop in obj) {
+                    if(obj.hasOwnProperty(prop))
+                        return false;
+                }
 
-            function toastInfo(message, title) {
-                toastr.info(message, title ? title : '');
-                logInfo('(toast) ' + message, title);
-            }
-
-            function toastWarning(message, title) {
-                toastr.warning(message, title ? title : '');
-                logWarn('(toast) ' + message, title);
-            }
-
-            function toastError(message, title) {
-                toastr.error(message, title ? title : '');
-                logError('(toast) ' + message, title);
+                return true;
             }
 
             // isMobile/isMobileOrTablet from @Michael Zaporozhets: http://stackoverflow.com/a/11381730/697370
             function isMobile() {
                 var check = false;
-                (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||$window.opera);
+                (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
                 return check;
             }
 
             function isMobileOrTablet() {
                 var check = false;
-                (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||$window.opera);
+                (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
                 return check;
             }
 
@@ -189,6 +159,10 @@
 
             function setLoggingStatus(isEnabled) {
                 enableDebugLogging = isEnabled;
+            }
+
+            function isEmptyObject(o) {
+                return o.isEmpty();
             }
         }
     ]);
@@ -254,6 +228,8 @@
     app.service('notify', [
         '$q', '$window',
         function($q, $window) {
+			// Default service operations to sane defaults
+			// in case no support is available.
             var svc = {
                 send: function(){},
                 requestPermission: function() {
@@ -333,32 +309,39 @@
 
     // Object functions
 
-    // Object.isEmpty()
-    if(!Object.prototype.isEmpty) {
-        Object.isEmpty = isEmpty;
-    }
-
     // Object.merge(o2)
     if(!Object.prototype.merge) {
-        Object.merge = merge;
+        Object.defineProperty(Object.prototype, 'merge', {
+            value: merge,
+            enumerable: false
+        });
     }
 
     // Array functions
 
     // Array.contains(needle)
     if(!Array.prototype.contains) {
-        Array.contains = contains;
+        Object.defineProperty(Array.prototype, 'contains', {
+            value: contains,
+            enumerable: false
+        });
     }
 
     // Array.intersect(array)
     // From @Paul S: http://stackoverflow.com/a/16227294/697370
     if(!Array.prototype.intersect) {
-        Array.intersect = intersect;
+        Object.defineProperty(Array.prototype, 'intersect', {
+            value: intersect,
+            enumerable: false
+        });
     }
 
     // Array.findIndex(value)
     if (!Array.prototype.findIndex) {
-        Array.findIndex = findIndex;
+        Object.defineProperty(Array.prototype, 'findIndex', {
+            value: findIndex,
+            enumerable: false
+        });
     }
 
 
@@ -366,60 +349,18 @@
 
     // Date.addDays(days)
     if(!Date.prototype.addDays) {
-        Date.addDays = addDays;
+        Object.defineProperty(Date.prototype, 'addDays', {
+            value: addDays,
+            enumerable: false
+        });
     }
 
     // Date.subDays(days)
     if(!Date.prototype.subDays) {
-        Date.subDays = subDays;
-    }
-
-    /**
-     * Adapted from @Sean Vieira: http://stackoverflow.com/a/4994244/697370
-     * Returns true if this is an empty object.
-     * @return boolean
-     */
-    function isEmpty() {
-        // null and undefined are "empty"
-        if (this == null) return true;
-
-        // Assume if it has a length property with a non-zero value
-        // that that property is correct.
-        if (this.length > 0)    return false;
-        if (this.length === 0)  return true;
-
-        // If it isn't an object at this point
-        // it is empty, but it can't be anything *but* empty
-        // Is it empty?  Depends on your application.
-        if (typeof this !== "object") return true;
-
-        if (Object.getOwnPropertyNames(this).length > 0) return false;
-
-        return true;
-    }
-
-    /**
-     * Adapted from @Sean Vieira: http://stackoverflow.com/a/4994244/697370
-     * Returns true if this is an empty object.
-     * @return boolean
-     */
-    function isEmpty() {
-        // null and undefined are "empty"
-        if (this == null) return true;
-
-        // Assume if it has a length property with a non-zero value
-        // that that property is correct.
-        if (this.length > 0)    return false;
-        if (this.length === 0)  return true;
-
-        // If it isn't an object at this point
-        // it is empty, but it can't be anything *but* empty
-        // Is it empty?  Depends on your application.
-        if (typeof this !== "object") return true;
-
-        if (Object.getOwnPropertyNames(this).length > 0) return false;
-
-        return true;
+        Object.defineProperty(Date.prototype, 'subDays', {
+            value: contains,
+            enumerable: false
+        });
     }
 
     // Perform shallow merging
@@ -434,7 +375,7 @@
     };
 
     /**
-     * Returns whether a given array contains a given value
+     * Returns whether a given object contains a given value
      * @param Object this
      * @param mixed needle
      * @return boolean
